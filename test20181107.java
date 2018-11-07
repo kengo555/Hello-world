70824c67f446870361702db36601f1aa32a09241
kengo555 <kengo.haraki@topgate.co.jp>
2018-11-07 13:35:53 +0900

    test20181107に「次に作った一文　12:32」を書いた

--- test20181107.java
+++ test20181107.java
@@ -3,6 +3,8 @@ package jp.co.topgate
 public class test20181107{
   public static void main (String[] args) {
     System.out.println("はじめに作った一文 12:54現在");
+
+    System.out.println("次に作った一文　13:32現在");
   }
 
 }
